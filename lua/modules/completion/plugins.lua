7d57fd0c310b824fb10681528f8805ec66f36c28
Nicholas Cristofaro <nicholaspcr@gmail.com>
2022-11-02 00:29:35 +0000

    Update completion module

--- lua/modules/completion/plugins.lua
+++ lua/modules/completion/plugins.lua
@@ -5,13 +5,30 @@
 local plugin = require('core.pack').register_plugin
 local conf = require('modules.completion.config')
 
+local enable_lsp_filetype = {
+  'go',
+  'lua',
+  'sh',
+  'rust',
+  'c',
+  'cpp',
+  'zig',
+  'typescript',
+  'typescriptreact',
+  'json',
+  'python',
+}
+
 plugin({
-  'neovim/nvim-lspconfig',
+  '~/Workspace/nvim-lspconfig',
+  -- 'neovim/nvim-lspconfig',
   -- used filetype to lazyload lsp
   -- config your language filetype in here
-  ft = { 'lua', 'rust', 'c', 'cpp' },
+  ft = enable_lsp_filetype,
   config = conf.nvim_lsp,
 })
+
+plugin({ '~/Workspace/lspsaga.nvim', after = 'nvim-lspconfig', config = conf.lspsaga })
 
 plugin({
   'hrsh7th/nvim-cmp',
@@ -26,3 +43,5 @@ plugin({
 })
 
 plugin({ 'L3MON4D3/LuaSnip', event = 'InsertCharPre', config = conf.lua_snip })
+
+plugin({ 'windwp/nvim-autopairs', event = 'InsertEnter', config = conf.auto_pairs })
