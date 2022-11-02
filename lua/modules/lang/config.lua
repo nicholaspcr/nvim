a20c4484da80d6590a7d5c3d58502660f336833c
Nicholas Cristofaro <nicholaspcr@gmail.com>
2022-11-02 00:29:11 +0000

    Update lang modules

--- lua/modules/lang/config.lua
+++ lua/modules/lang/config.lua
@@ -3,9 +3,35 @@ local config = {}
 function config.nvim_treesitter()
   vim.api.nvim_command('set foldmethod=expr')
   vim.api.nvim_command('set foldexpr=nvim_treesitter#foldexpr()')
+
+  local ignored = {
+    'phpdoc',
+    'astro',
+    'beancount',
+    'bibtex',
+    'bluprint',
+    'eex',
+    'embedded_template',
+    'vala',
+    'wgsl',
+    'verilog',
+    'twig',
+    'turtle',
+    'm68k',
+    'hocon',
+    'lalrpop',
+    'meson',
+    'mehir',
+    'rasi',
+    'rego',
+    'racket',
+    'pug',
+    'java',
+  }
+
   require('nvim-treesitter.configs').setup({
     ensure_installed = 'all',
-    ignore_install = { 'phpdoc' },
+    ignore_install = ignored,
     highlight = {
       enable = true,
     },
