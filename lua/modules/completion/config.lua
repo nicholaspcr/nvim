07f21c2bcb86732b7d1f2d5f6003d679e5751f25
Nicholas Cristofaro <nicholaspcr@gmail.com>
2022-11-08 22:03:59 -0300

    Add more config to completion module

--- lua/modules/completion/config.lua
+++ lua/modules/completion/config.lua
@@ -9,6 +9,15 @@ function config.nvim_lsp()
   require('modules.completion.lspconfig')
 end
 
+function config.lspsaga()
+  local saga = require('lspsaga')
+  saga.init_lsp_saga({
+    symbol_in_winbar = {
+      enable = true,
+    },
+  })
+end
+
 function config.nvim_cmp()
   local cmp = require('cmp')
 
@@ -17,6 +26,25 @@ function config.nvim_cmp()
     window = {
       completion = cmp.config.window.bordered(),
       documentation = cmp.config.window.bordered(),
+    },
+    formatting = {
+      fields = { 'abbr', 'kind', 'menu' },
+    },
+    -- You can set mappings if you want
+    mapping = cmp.mapping.preset.insert({
+      ['<C-e>'] = cmp.config.disable,
+      ['<CR>'] = cmp.mapping.confirm({ select = true }),
+    }),
+    snippet = {
+      expand = function(args)
+        require('luasnip').lsp_expand(args.body)
+      end,
+    },
+    sources = {
+      { name = 'nvim_lsp' },
+      { name = 'luasnip' },
+      { name = 'path' },
+      { name = 'buffer' },
     },
   })
 end
@@ -41,6 +69,17 @@ function config.lua_snip()
   require('luasnip.loaders.from_vscode').lazy_load({
     paths = { './snippets/' },
   })
+end
+
+function config.auto_pairs()
+  require('nvim-autopairs').setup({})
+  local status, cmp = pcall(require, 'cmp')
+  if not status then
+    vim.cmd([[packadd nvim-cmp]])
+    cmp = require('cmp')
+  end
+  local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+  cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({ map_char = { tex = '' } }))
 end
 
 return config
