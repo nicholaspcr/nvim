993645ce3c9fce2a6c9a19503b5bdd8c239857e6
Nicholas Cristofaro <nicholaspcr@gmail.com>
2023-08-08 10:26:32 -0300

    Add custom shortcut to alternate between file and file_test in golang

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -3,3 +3,9 @@ vim.opt_local.expandtab = false
 vim.opt_local.shiftwidth = 4
 vim.opt_local.softtabstop = 4
 vim.opt_local.tabstop = 4
+
+local keymap = require('core.keymap')
+local nmap = keymap.nmap
+local cmd, opts = keymap.cmd, keymap.new_opts
+local noremap, silent =  keymap.noremap, keymap.silent
+nmap({ '<Leader>ga', cmd(':GoAlternate'), opts(noremap) })
