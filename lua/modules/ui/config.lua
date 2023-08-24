b7e7ed5403be83696838b16271c87f3548576aa2
Nicholas Cristofaro <nicholaspcr@gmail.com>
2023-08-24 19:40:39 -0300

    Add bufferline plugin to UI module

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -131,4 +131,85 @@ function config.todo()
   }
 end
 
+function config.gitsigns()
+  require('gitsigns').setup{}
+end
+
+function config.bufferline()
+  vim.opt.termguicolors = true
+  require("bufferline").setup{
+    options = {
+      mode = "tabs",
+    },
+  }
+end
+
+-- Colorscheme configuration
+function config.gruvbox()
+    vim.cmd('colorscheme gruvbox-baby')
+end
+
+function config.tokyonight()
+    require("tokyonight").setup({
+      style = "storm", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+      light_style = "dark", -- The theme is used when the background is set to light
+      transparent = true, -- Enable this to disable setting the background color
+      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
+      styles = {
+        -- Style to be applied to different syntax groups
+        -- Value is any valid attr-list value for `:help nvim_set_hl`
+        comments = { italic = true },
+        keywords = { italic = true },
+        functions = {},
+        variables = {},
+        -- Background styles. Can be "dark", "transparent" or "normal"
+        sidebars = "dark", -- style for sidebars, see below
+        floats = "dark", -- style for floating windows
+      },
+
+      -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
+      sidebars = { "qf", "help" }, 
+
+      -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
+      day_brightness = 0.3, 
+
+      -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work
+      -- with the standard **StatusLine** and **LuaLine**.
+      hide_inactive_statusline = false, 
+
+      dim_inactive = false, -- dims inactive windows
+      lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
+
+      -- You can override specific color groups to use other groups or a hex color
+      -- function will be called with a ColorScheme table
+      -- @param colors ColorScheme
+      on_colors = function(colors) end,
+
+      -- You can override specific highlights to use other groups or a hex color
+      -- function will be called with a Highlights and ColorScheme table
+      --@param highlights Highlights
+      --@param colors ColorScheme
+      on_highlights = function(highlights, colors) end,
+    })
+
+    -- Sets colorscheme
+    vim.cmd('colorscheme tokyonight')
+  end
+
+  function config.kanagawa()
+    require('kanagawa').setup({
+      compile = true,             -- enable compiling the colorscheme
+      undercurl = true,            -- enable undercurls
+      commentStyle = { italic = true },
+      keywordStyle = { italic = true},
+      statementStyle = { bold = true },
+      transparent = true,         -- do not set background color
+      dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
+      terminalColors = true,       -- define vim.g.terminal_color_{0,17}
+    })
+
+    -- Sets colorscheme
+    vim.cmd('colorscheme tokyonight')
+  end
+
 return config
--- lua/modules/ui/package.lua
+++ lua/modules/ui/package.lua
@@ -11,7 +11,7 @@ package({
 package({
   'nvim-lualine/lualine.nvim',
   config = conf.lualine,
-  dependencies = {{ 'kyazdani42/nvim-web-devicons', opt = true }},
+  dependencies = {{'nvim-tree/nvim-web-devicons', opt = true }},
 })
 
 package({
@@ -20,52 +20,30 @@ package({
   config = conf.todo,
 })
 
+package({
+  'lewis6991/gitsigns.nvim',
+  config = conf.gitsigns,
+})
+
+package({
+  'nvim-tree/nvim-web-devicons',
+  config = function() require'nvim-web-devicons'.setup() end,
+})
+
+package({
+  'akinsho/bufferline.nvim', 
+  version = "*", 
+  dependencies = {{'nvim-tree/nvim-web-devicons', opt = true}},
+  config = conf.bufferline,
+})
+
+
+-- Packages related to ColorScheme
+-- Configs are commented in order to leave just the default settings be set.
 
 package({
   'folke/tokyonight.nvim',
-  config = function()
-    require("tokyonight").setup({
-      style = "moon", -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
-      light_style = "dark", -- The theme is used when the background is set to light
-      transparent = true, -- Enable this to disable setting the background color
-      terminal_colors = true, -- Configure the colors used when opening a `:terminal` in Neovim
-      styles = {
-        -- Style to be applied to different syntax groups
-        -- Value is any valid attr-list value for `:help nvim_set_hl`
-        comments = { italic = true },
-        keywords = { italic = true },
-        functions = {},
-        variables = {},
-        -- Background styles. Can be "dark", "transparent" or "normal"
-        sidebars = "dark", -- style for sidebars, see below
-        floats = "dark", -- style for floating windows
-      },
-
-      -- Set a darker background on sidebar-like windows. For example: `["qf", "vista_kind", "terminal", "packer"]`
-      sidebars = { "qf", "help" }, 
-
-      -- Adjusts the brightness of the colors of the **Day** style. Number between 0 and 1, from dull to vibrant colors
-      day_brightness = 0.3, 
-
-      -- Enabling this option, will hide inactive statuslines and replace them with a thin border instead. Should work
-      -- with the standard **StatusLine** and **LuaLine**.
-      hide_inactive_statusline = false, 
-
-      dim_inactive = false, -- dims inactive windows
-      lualine_bold = false, -- When `true`, section headers in the lualine theme will be bold
-
-      -- You can override specific color groups to use other groups or a hex color
-      -- function will be called with a ColorScheme table
-      -- @param colors ColorScheme
-      on_colors = function(colors) end,
-
-      -- You can override specific highlights to use other groups or a hex color
-      -- function will be called with a Highlights and ColorScheme table
-      --@param highlights Highlights
-      --@param colors ColorScheme
-      on_highlights = function(highlights, colors) end,
-    })
-  end
+  config = conf.tokyonight,
 })
 
 package({
@@ -75,30 +53,6 @@ package({
 
 package({
   'rebelot/kanagawa.nvim',
-  config = function()
-    require('kanagawa').setup({
-      compile = false,             -- enable compiling the colorscheme
-      undercurl = true,            -- enable undercurls
-      commentStyle = { italic = true },
-      functionStyle = {},
-      keywordStyle = { italic = true},
-      statementStyle = { bold = true },
-      typeStyle = {},
-      transparent = false,         -- do not set background color
-      dimInactive = false,         -- dim inactive window `:h hl-NormalNC`
-      terminalColors = true,       -- define vim.g.terminal_color_{0,17}
-      colors = {                   -- add/modify theme and palette colors
-          palette = {},
-          theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
-      },
-      overrides = function(colors) -- add/modify highlights
-          return {}
-      end,
-      theme = "wave",              -- Load "wave" theme when 'background' option is not set
-      background = {               -- map the value of 'background' option to a theme
-          dark = "dragon",           -- try "dragon" !
-          light = "lotus"
-      },
-    })
-  end
+  --config = conf.kanagawa,
 })
+
