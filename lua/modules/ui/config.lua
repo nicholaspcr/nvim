4059095f2f3753b1eb0448bcd94ee1ed6c9eb9d3
Nicholas Cristofaro <nicholaspcr@gmail.com>
2023-09-01 11:43:16 -0300

    Add proper symbols to lualine diagnostics

--- lua/modules/ui/config.lua
+++ lua/modules/ui/config.lua
@@ -43,7 +43,16 @@ function config.lualine()
     },
     sections = {
       lualine_a = {'mode'},
-      lualine_b = {'branch', 'diff', 'diagnostics'},
+      lualine_b = {
+        'branch', 
+        'diff', 
+        {
+          'diagnostics',
+          sections = {'error', 'warn', 'info', 'hint'},	
+          colored = true,
+          symbols = {error = ' ', warn = ' ', info = ' ', hint = ''},
+        },
+      },
       lualine_c = {{'filename',path=1,}},
       lualine_x = {'encoding', 'fileformat', 'filetype'},
       lualine_y = {'progress'},
