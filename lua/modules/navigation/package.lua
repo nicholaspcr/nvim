30496f802113e1139103e8730c952b3a00c19b27
Nicholas Cristofaro <nicholaspcr@gmail.com>
2023-09-03 13:14:51 -0300

    Add git worktree plugin

--- lua/modules/navigation/package.lua
+++ lua/modules/navigation/package.lua
@@ -7,16 +7,14 @@ package({
   dependencies = {
     { 'nvim-lua/plenary.nvim' },
     { 'nvim-telescope/telescope-fzy-native.nvim' },
-    { 'nvim-telescope/telescope-file-browser.nvim' }
-    -- { 'kdheepak/lazygit.nvim' }
+    { 'nvim-telescope/telescope-file-browser.nvim' },
+    { 'ThePrimeagen/git-worktree.nvim' },
+    { 'folke/todo-comments.nvim' },
   },
   config = conf.telescope,
   init = function()
-    -- To get telescope-file-browser loaded and working with telescope,
-    -- you need to call load_extension, somewhere after setup function:
     require("telescope").load_extension "file_browser"
-    --require("telescope").load_extension "lazygit"
-
+    require("telescope").load_extension "git_worktree"
 
     vim.g.mapleader = ' '
     local keymap = require('core.keymap')
@@ -24,15 +22,23 @@ package({
     local cmd, opts = keymap.cmd, keymap.new_opts
     local noremap, silent =  keymap.noremap, keymap.silent
 
-
     -- Telescope mappings
+    local extensions = require('telescope').extensions
     nmap({
+      -- Buffer related mappings
       { '<Leader>b', cmd('Telescope buffers'), opts(noremap, silent) },
+
+      -- File related mappings
       { '<Leader>fa', cmd('Telescope live_grep'), opts(noremap, silent) },
       { '<Leader>ff', cmd('Telescope find_files'), opts(noremap, silent) },
-      { '<Leader>fw', cmd('Telescope file_browser'), opts(noremap, silent) },
       { '<Leader>fl', cmd('Telescope file_browser path=%:p:h select_buffer=true'), opts(noremap, silent) },
+
+      -- Todo related mappings
       { '<Leader>ft', cmd('TodoTelescope'), opts(noremap, silent) },
+
+      -- Worktree related mappings
+      { '<Leader>fwl', extensions.git_worktree.git_worktrees, opts(noremap, silent) },
+      { '<Leader>fwc', extensions.git_worktree.create_git_worktree, opts(noremap, silent) },
     })
   end
 })
@@ -43,7 +49,6 @@ package({
   'ThePrimeagen/harpoon',
   dependencies = { {'nvim-lua/plenary.nvim'},},
   init = function()
-    vim.g.mapleader = ' '
     local keymap = require('core.keymap')
     local nmap = keymap.nmap
     local cmd, opts = keymap.cmd, keymap.new_opts
@@ -57,6 +62,13 @@ package({
       { '<C-l>', function() require('harpoon.ui').nav_file(4) end, opts(noremap, silent) },
     })
   end
+})
+
+package({
+  'ThePrimeagen/git-worktree.nvim',
+  config = function()
+    require("git-worktree").setup()
+  end,
 })
 
 package ({
