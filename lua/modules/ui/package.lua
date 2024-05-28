877a35e51c84adcb898fa275d23eedf72a0f5eda
nicholas cristofaro <nicholas.cristofaro@thethingsindustries.com>
2024-05-28 17:57:53 -0300

    Add markdown preview plugin

--- lua/modules/ui/package.lua
+++ lua/modules/ui/package.lua
@@ -53,3 +53,10 @@ package({
   name = "catppuccin",
   config = conf.catppuccin,
 })
+
+package({
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+  ft = { "markdown" },
+  build = function() vim.fn["mkdp#util#install"]() end,
+})
