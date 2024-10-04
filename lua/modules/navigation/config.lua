6be6d507775004c3dbe1af684319b2ea32cb8190
Nicholas Cristofaro <nicholas.cristofaro@thethingsindustries.com>
2024-10-04 17:59:34 -0300

    Update obisidian plugin to be a single folder

--- lua/modules/navigation/config.lua
+++ lua/modules/navigation/config.lua
@@ -126,12 +126,8 @@ function config.obsidian()
   require("obsidian").setup({
       workspaces = {
         {
-          name = "personal",
-          path = "~/vaults/personal",
-        },
-        {
-          name = "work",
-          path = "~/vaults/work",
+          name = "notes",
+          path = "~/vaults/notes",
         },
       },
       notes_subdir = "notes",
