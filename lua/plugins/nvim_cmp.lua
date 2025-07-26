900544b682fc5b3e5c9b50dfaab82afd3c18c8ac
Nicholas Cristofaro <nicholaspcr@gmail.com>
2025-07-26 13:40:54 -0300

    Remove unecessary config from nvim_cmp

--- lua/plugins/nvim_cmp.lua
+++ lua/plugins/nvim_cmp.lua
@@ -35,17 +35,6 @@ local function nvim_cmp()
   capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
   capabilities.offsetEncoding = 'utf-8'
 
-  local signs = {
-    Error = ' ',
-    Warn = ' ',
-    Info = ' ',
-    Hint = ' ',
-  }
-  for type, icon in pairs(signs) do
-    local hl = 'DiagnosticSign' .. type
-    vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
-  end
-
   vim.diagnostic.config({
     signs = true,
     update_in_insert = true,
