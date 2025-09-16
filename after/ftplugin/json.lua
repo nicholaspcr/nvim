a8a938f7139ff2b553c371d668319c62cc3df112
Nicholas Cristofaro <nicholaspcr@gmail.com>
2025-09-16 19:10:14 -0300

    Update tab spacing

--- after/ftplugin/json.lua
+++ after/ftplugin/json.lua
@@ -5,4 +5,4 @@ vim.opt_local.foldmethod = 'syntax'
 vim.opt_local.formatoptions = 'tcq2l'
 vim.opt_local.shiftwidth = 4
 vim.opt_local.softtabstop = 4
-vim.opt_local.tabstop = 8
+vim.opt_local.tabstop = 4
