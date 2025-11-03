6318d5289f82458d02a915f740715a77b8e25cb5
Nicholas Cristofaro <nicholaspcr@gmail.com>
2025-11-03 19:30:38 +0000

    chore: remove unused plugin and optimize spell checking
    - Remove catppuccin plugin (never activated)
    - Move spell checking from global to filetype-specific
    - Add spell checking to markdown.lua and text.lua ftplugin
    - Prevents spell checking in code files for better performance
    - Document NVIM_COLUMN environment variable

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -4,3 +4,7 @@ vim.opt_local.smartindent = true
 vim.opt_local.shiftwidth = 4
 vim.opt_local.softtabstop = 4
 vim.opt_local.tabstop = 4
+
+-- Enable spell checking for markdown files
+vim.opt_local.spell = true
+vim.opt_local.spelllang = "en_us"
---
+++ after/ftplugin/text.lua
@@ -0,0 +1,3 @@
+-- Enable spell checking for text files
+vim.opt_local.spell = true
+vim.opt_local.spelllang = "en_us"
