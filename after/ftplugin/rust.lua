a02bd227eff3f78449fa564e38dd5b4fbf38d8be
Nicholas Cristofaro <nicholaspcr@gmail.com>
2026-01-14 16:10:24 -0300

    Add format on save for Rust files

--- after/ftplugin/rust.lua
+++ after/ftplugin/rust.lua
@@ -5,3 +5,11 @@ vim.opt_local.smartindent = true
 vim.opt_local.tabstop = 4
 vim.opt_local.cindent = false
 vim.opt_local.expandtab = true
+
+-- Format on save
+vim.api.nvim_create_autocmd('BufWritePre', {
+    buffer = 0,
+    callback = function()
+        vim.lsp.buf.format({ async = false })
+    end,
+})
