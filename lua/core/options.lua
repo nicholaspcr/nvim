efb4a18b625173f480efaa555654a94442c57b01
Nicholas Cristofaro <nicholas.cristofaro@thethingsindustries.com>
2026-02-05 19:54:48 -0300

    fix: restore dark background

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,7 +1,7 @@
 local opt = vim.opt
 local cache_dir = vim.env.HOME .. '/.cache/nvim/'
 
-opt.background = 'light'
+opt.background = 'dark'
 opt.termguicolors = true
 opt.hidden = true
 opt.magic = true
