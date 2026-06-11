0acc271fc2cfbba6f85a4549f345904b26ba8fad
Nicholas Cristofaro <nicholas.cristofaro@thethingsindustries.com>
2026-06-11 17:28:35 -0300

    fix: replace deprecated vim.highlight.on_yank with vim.hl

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -122,7 +122,7 @@ opt.colorcolumn = '+1'
 vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('highlight_yank', { clear = true }),
   callback = function()
-    vim.highlight.on_yank({ higroup = 'IncSearch', timeout = 150 })
+    vim.hl.on_yank({ higroup = 'IncSearch', timeout = 150 })
   end,
 })
 
