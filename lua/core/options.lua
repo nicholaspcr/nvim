4e2a44d71904775f9d1268d8c23b5a2a06a47672
Nicholas Cristofaro <nicholas.cristofaro@thethingsindustries.com>
2026-06-11 17:17:25 -0300

    fix: shim get_node_text for nvim-treesitter on nvim 0.12

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -1,6 +1,21 @@
 local opt = vim.opt
 local cache_dir = vim.env.HOME .. '/.cache/nvim/'
 
+-- Compat shim for nvim-treesitter (master) on Neovim 0.12:
+-- match[capture_id] is now TSNode[] instead of TSNode, so directives like
+-- `set-lang-from-info-string!` pass a list to get_node_text, which then errors
+-- with "attempt to call method 'range' (a nil value)". Unwrap to first node.
+do
+  local orig_get_node_text = vim.treesitter.get_node_text
+  vim.treesitter.get_node_text = function(node, source, opts)
+    if type(node) == 'table' and node.range == nil then
+      node = node[1]
+      if node == nil then return '' end
+    end
+    return orig_get_node_text(node, source, opts)
+  end
+end
+
 opt.background = 'dark'
 opt.termguicolors = true
 opt.virtualedit = 'block'
